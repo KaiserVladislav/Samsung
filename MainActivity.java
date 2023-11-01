436b5a9348ebbb9286ffac4a1c8f7fd3a38d3223
Vladislav Shendrik <60485560+KaiserVladislav@users.noreply.github.com>
2023-11-02 00:23:25 +0300

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,19 +1,17 @@
-package com.example.shendrik_cells;
+package com.example.samsung;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.GridLayout;
 import android.widget.TextView;
 import android.widget.Toast;
-
-/*TODO: minefield I
-*  1) placing mines
-*  2) */
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,61 +20,106 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mines_status = findViewById(R.id.TV);
-        mines_status.setText(""+MINESCONST+"/"+MinesCurrent);
         generate();
     }
 
-    // declaration
-    TextView mines_status;
-    final int MINESCONST=3;
-    int MinesCurrent=3;
-    Button cells[][];
-    boolean cells_mines[][];
-    final int WIDTH=10;
-    final int HEIGHT=10;
+    // TODO: DECLARATION OF CONSTANTS
+    final int MINESCONST = 3;
+    int MinesCurrent = 3;
+    final int WIDTH = 9;
+    final int HEIGHT = 9;
 
-    public void generate(){
+    public void generate() {
+
+        // TODO: SECONDARY DECLARATION
         GridLayout layout = findViewById(R.id.GRID);
         layout.removeAllViews();
         layout.setColumnCount(WIDTH);
         LayoutInflater inflater = (LayoutInflater) getApplicationContext().getSystemService(LAYOUT_INFLATER_SERVICE);
 
+        Drawable button_pressed_red = getApplicationContext().getResources().getDrawable(R.drawable.button_red_gradient);
+        Drawable button_unpressed_golden = getApplicationContext().getResources().getDrawable(R.drawable.button_golden_gradient);
+        Drawable button_pressed_blue = getApplicationContext().getResources().getDrawable(R.drawable.button_blue_gradient);
+
+        Button cells[][];
         cells = new Button[HEIGHT][WIDTH];
-        cells_mines=new boolean[HEIGHT][WIDTH];
-        for(int i=0;i<HEIGHT;i++){
-            for (int j = 0; j < WIDTH; j++) {
-                cells[i][j]= (Button) inflater.inflate(R.layout.cell,layout,false);
+        int minefield[][] = MineSweeperFunctional.generateMineField(cells);
+        final int[] minesAmount = {0};
+        for (int i = 0; i < minefield.length; i++) {
+            for (int j = 0; j < minefield[i].length; j++) {
+                if(minefield[i][j]==1){
+                    minesAmount[0]++;
+                }
             }
         }
-        for(int i=0;i<HEIGHT;i++){
-            for (int j = 0; j < WIDTH; j++) {
-                cells[i][j].setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
 
-                        view.setBackgroundColor(getColor(R.color.red));
+        TextView game_status;
+        game_status = findViewById(R.id.TV);
+        game_status.setText(R.string.status_ongoing);
+
+        TextView mines_ratio;
+        mines_ratio = findViewById(R.id.TV2);
+        mines_ratio.setText(R.string.placeholder);
+
+
+        for (int i = 0; i < HEIGHT; i++) { // TODO: Creating buttons (cell.xml type) on layout (Gridlayout class)
+            for (int j = 0; j < WIDTH; j++) {
+                cells[i][j] = (Button) inflater.inflate(R.layout.cell, layout, false);
+            }
+        }
+
+        // TODO: MAIN BODY
+        for (int i = 0; i < HEIGHT; i++) {
+            for (int j = 0; j < WIDTH; j++) {
+                int finalI = i;
+                int finalJ = j;
+
+                cells[i][j].setOnClickListener(new View.OnClickListener() {// TODO: [clearing the tile -  pressed mine? IMMEDIATE_DETONATION : clearing area around] setting button red;
+                    public void onClick(View view) {
+                        cells[finalI][finalJ].setBackground(button_pressed_red);
+                        if (minefield[finalI][finalJ]==1){
+                            MineSweeperFunctional.detonating(MainActivity.this);
+                            game_status.setText(R.string.status_defeat);
+
+                        }else{
+                            int temp[] = MineSweeperFunctional.clearing(minefield,finalI,finalJ);
+                            cells[temp[0]][temp[1]].setText(temp[2]+"");
+                        }
                     }
                 });
-                cells[i][j].setOnLongClickListener(new View.OnLongClickListener() {
+
+                cells[i][j].setOnLongClickListener(new View.OnLongClickListener() { // TODO: [marking the tile with mine flag - purely dysfunctional] setting button blue;
                     @Override
                     public boolean onLongClick(View v) {
-                        v.setBackgroundColor(Color.BLUE);
-
-
-//                        if(MinesCurrent==0){
-//                            Toast.makeText(getApplicationContext(),"WIN",Toast.LENGTH_LONG).show();
-//                        }
+                        cells[finalI][finalJ].setBackground(button_pressed_blue);
+                        if (minefield[finalI][finalJ]==1){
+                            minesAmount[0] -=1;
+                        }
+                        if (minesAmount[0]==0){
+                            MineSweeperFunctional.win(MainActivity.this);
+                            game_status.setText(R.string.status_victory);
+                        }
+                        mines_ratio.setText(minesAmount[0]+"");
                         return true;
                     }
                 });
 
-                cells[i][j].setText((i+1)+" "+(j+1));
-                cells[i][j].setTag(""+j+HEIGHT*(i+1));
+                // TODO: SETTING TEXT
+                //cells[i][j].setText(i + " " + j);
+                //cells[i][j].setTag(i + " " + j);
+
+
                 layout.addView(cells[i][j]);
+                Log.i("", minefield[i][j] + " " + (i+1) + " " + (j+1));
 
             }
         }
 
+
     }
+    public void reload(){ // I dunno man
+        generate();
+    }
+
+
 }
---
+++ MineSweeperFunctional.java
@@ -0,0 +1,66 @@
+package com.example.samsung;
+
+import android.content.Context;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.Random;
+
+public class MineSweeperFunctional {
+    public static int[][]  generateMineField(Button[][] cells){
+        int mineField[][]=new int[cells.length][cells[0].length];
+        Random random = new Random(); // todo: target mines amount ~ 8 (of 81 cells)
+        int limit=9;
+        int counter=0;
+        for (int i = 0; i < mineField.length; i++) {
+            for (int j = 0; j < mineField[i].length; j++) {
+                mineField[i][j]= (random.nextInt(100)==8)?1:0; // pls change later
+            }
+        }
+
+        return mineField;
+    }
+
+    public static void detonating(Context context){
+        Toast.makeText(context, R.string.status_defeat,Toast.LENGTH_SHORT).show();
+    }
+    public static void win(Context context){
+        Toast.makeText(context, R.string.status_victory,Toast.LENGTH_SHORT).show();
+    }
+
+
+    public static int[] clearing(int[][] minefield, int i, int j){
+        int mines_around=0;
+        int index_i=i;
+        int index_j=j;
+        int[][] to_clear;
+
+        while(mines_around==0){
+
+            for (int k = 0; k < 3; k++) {
+                for (int l = 0; l < 3; l++) {
+                    try{
+                        if (minefield[index_i-1+k][index_j-1+l] ==1){
+                            mines_around++;
+                        }
+                    }catch (ArrayIndexOutOfBoundsException idontcare){
+                        continue;
+                    }
+
+                }
+            }
+            if (mines_around==0)
+            {
+                index_i+=1;
+            }
+
+        }
+        int[] result = new int[3];
+        result[0]=index_i;
+        result[1]=index_j;
+        result[2]=mines_around;
+        return result;
+    }
+
+
+}
